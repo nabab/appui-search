9a2793b374f9fa7d6ae24bdff2700226d29ae226
nabab <thomas.nabet@gmail.com>
2022-06-02 02:40:07 +0200

    Removed client stuff from default search

--- src/components/result/item/item.js
+++ src/components/result/item/item.js
@@ -1,10 +1,15 @@
 (() => {
   return {
     mixins: [bbn.vue.basicComponent],
-    props: ['source'],
-    computed: {
-      cls() {
-        return appui.app.get_adherent_class(this.source.statut, this.source.statut_prospect ? this.source.statut_prospect : '') || '';
+    props: {
+      source: {
+        type: Object,
+        required: true
+      /*},
+      url: {
+        type: String,
+        required: true
+      */
       }
     },
     methods: {
