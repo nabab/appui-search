593fe0b544e27cc35c7e1d53e786a068932d7595
nabab <thomas.nabet@gmail.com>
2025-02-07 03:26:26 +0100

    Better perf

--- src/mvc/public/start.php
+++ src/mvc/public/start.php
@@ -8,10 +8,12 @@ if (!empty($ctrl->post['uid']) && !empty($ctrl->post['conditions'])) {
   $res = ['success' => false, 'errors' => []];
   $ctrl->setStream();
   $file = $ctrl->dataPath() . $ctrl->post['uid'] . '.json';
-  $log = $ctrl->dataPath() . $ctrl->post['uid'];
+  $logFile = $ctrl->dataPath() . $ctrl->post['uid'];
   file_put_contents($file, json_encode($ctrl->post['conditions'][0]));
   $res['success'] = true;
   $i = 0;
+  $max = 50;
+  $maxWorkers = 5;
   $workers = [];
   $desc = [
     ["pipe", "r"],  // stdin is a pipe that the child will read from
@@ -20,94 +22,151 @@ if (!empty($ctrl->post['uid']) && !empty($ctrl->post['conditions'])) {
   ];
   $current = null;
   $cmd = 'php -f router.php %s "%s"';
-  while (file_exists($file)) {
-    $condition = json_decode(file_get_contents($file), true);
+  $isOk = function($condition) use ($file) {
+    if (!file_exists($file)) {
+      return false;
+    }
+    $tmp = json_decode(file_get_contents($file), true);
+    if ($tmp['value'] !== $condition['value']) {
+      return 0;
+    }
+
+    return 1;
+  };
+  $log = function($msg) {
+    X::log($msg, 'search');
+  };
+  $rm = function(&$workers) {
+    while (count($workers)) {
+      $w = array_shift($workers);
+      $status = proc_get_status($w['proc']);
+      if ($status['running']) {
+        proc_terminate($w['proc']);
+      }
+
+      proc_close($w['proc']);
+      unlink($w['log']);
+    }
+  };
+  $root = $ctrl->appPath();
+  $url = $ctrl->pluginUrl('appui-search') . '/results';
+  $addWorker = function(&$workers, $result, $step) use ($cmd, $root, $desc, $logFile, $url) {
+    $w = [
+      'proc' => null,
+      'id' => $result['id'],
+      'cmd' => sprintf(
+        $cmd,
+        $url,
+        bbn\Str::escapeDquotes(json_encode(['item' => $result['item'], 'step' => $step]))
+      ),
+      'uid' => Str::genpwd(),
+      'pipes' => []
+    ];
+    $w['log'] = $logFile . '-' . $w['uid'] . '.log';
+    file_put_contents($w['log'], '');
+    $wdesc = $desc;
+    $wdesc[2][1] = $w['log'];
+    $w['proc'] = proc_open(
+      $w['cmd'],
+      $wdesc,
+      $w['pipes'],
+      $root
+    );
+    stream_set_blocking($w['pipes'][1], 0);
+    $workers[] = $w;
+  };
+
+  $tot = 0;
+  $condition = json_decode(file_get_contents($file), true);
+  $timer = new bbn\Util\Timer();
+  $last = null;
+  while ($isOk($condition) !== false) {
     if (!empty($condition['value'])) {
+      // Starting a new cycle with a new value
       if ($current !== $condition['value']) {
         $current = $condition['value'];
-        while (count($workers)) {
-          $w = array_shift($workers);
-          proc_close($w['proc']);
-          unlink($w['log']);
-        }
+        $rm($workers);
 
         $step = 0;
+        $tot = 0;
         $results = [];
-        while (($result = $ctrl->getModel(['value' => $condition['value'], 'step' => $step]))
-            && !empty($result['item'])
-          && ($step < 100)
-        ) {
-          ob_end_clean();
-          $w = [
-            'proc' => null,
-            'id' => $result['id'],
-            'cmd' => sprintf(
-              $cmd,
-              $ctrl->pluginUrl('appui-search') . '/results',
-              bbn\Str::escapeDquotes(json_encode(['item' => $result['item'], 'step' => $step]))
-            ),
-            'uid' => Str::genpwd(),
-            'pipes' => []
-          ];
-          $w['log'] = $log . $w['uid'] . '.log';
-          file_put_contents($w['log'], '');
-          $wdesc = $desc;
-          $wdesc[2][1] = $w['log'];
-          $w['proc'] = proc_open(
-            $w['cmd'],
-            $wdesc,
-            $w['pipes'],
-            $ctrl->appPath()
-          );
-          stream_set_blocking($w['pipes'][1], 0);
-          $workers[] = $w;
-          $step++;
+      }
+
+
+      if ($tot < $max) {
+        while ((count($workers) <= $maxWorkers) && ($result = $ctrl->getModel(['value' => $condition['value'], 'step' => $step]))) {
+          $addWorker($workers, $result, $step);
+          if (!file_exists($file)) {
+            $rm($workers);
+            break;
+          }
+          elseif (!empty($result['next_step'])) {
+            $step = $result['next_step'];
+          }
+          else {
+            break;
+          }
         }
 
-      }
-      
-        ob_end_clean();
+        // Going through the workers until extinction
+        for ($j = 0; $j < count($workers); $j++) {
+          if ($isOk($condition)) {
+            $w = $workers[$j];
+            $status = proc_get_status($w['proc']);
+            if (!$status['running']) {
+              $tmp = stream_get_contents($w['pipes'][1]);
+              if ($tmp && Str::isJson($tmp)) {
+                $ret = json_decode($tmp, true);
+                if (!empty($ret['data']['results'])) {
+                  $tot += count($ret['data']['results']);
+                  if ($tot > $max) {
+                    array_splice($ret['data']['results'], count($ret['data']['results']) - ($tot - $max));
+                  }
+                  $ctrl->stream([
+                    'data' => $ret['data']['results'],
+                    'step' => $ret['step'],
+                    'item' => $ret['item'],
+                    'id' => $w['id']
+                  ]);
+                }
+              }
 
-      for ($j = 0; $j < count($workers); $j++) {
-        $w = $workers[$j];
-        $status = proc_get_status($w['proc']);
-        if (!$status['running']) {
-          $tmp = stream_get_contents($w['pipes'][1]);
-          if ($tmp && Str::isJson($tmp)) {
-            $ret = json_decode($tmp, true);
-            if (!empty($ret['data']['results'])) {
-              $ctrl->stream([
-                'data' => $ret['data']['results'],
-                'step' => $ret['step'],
-                'item' => $ret['item'],
-                'id' => $w['id']
-              ]);
+              if (file_exists($w['log'])) {
+                $err = file_get_contents($w['log']);
+                if ($err) {
+                  $res['errors'][] = $err;
+                  $ctrl->stream(['error' => $err, 'command' => $w['cmd']]);
+                }
+              }
+
+              if ($tot > $max) {
+                $rm($workers);
+                break;
+              }
+              else {
+                array_splice($workers, $j, 1);
+                $j--;
+                $arr = [$w];
+                $rm($arr);
+              }
             }
           }
-
-          if (file_exists($w['log'])) {
-            $err = file_get_contents($w['log']);
-            if ($err) {
-              $res['errors'][] = $err;
-              $ctrl->stream(['error' => $err, 'command' => $w['cmd']]);
-            }
-        
+          else {
+            $condition = json_decode(file_get_contents($file), true);
+            $rm($workers);
+            break;
           }
-
-          array_splice($workers, $j, 1);
-          $j--;
-          proc_close($w['proc']);
-          unlink($w['log']);
         }
       }
     }
   
-    usleep(100000);
+    usleep(50000);
     $i++;
   }
 
-  while (count($workers)) {
-    proc_close(array_shift($workers));
+  $rm($workers);
+  if (file_exists($file)) {
+    unlink($file);
   }
 
   $res['progress'] = $i;
